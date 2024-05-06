31ba1e84016b52388931b5eed6e846411e3757b8
Paul <awolesiboluwatife@gmail.com>
2024-05-06 12:39:25 +0200

    add suspense element

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AnimatedCursor from 'react-animated-cursor';
 import Wrapper from "@/components/Wrapper"
+import { Suspense } from 'react';
 
 
 
@@ -16,24 +17,26 @@ export default function RootLayout({ children }) {
   return (
 		<html lang="en">
 			<body className={inter.className}>
-				<AnimatedCursor
-					innerSize={8}
-          outerSize={35}
-          innerScale={1}
-          outerScale={2}
-          outerAlpha={0}
-          hasBlendMode={true}
-          innerStyle={{
-            backgroundColor: 'rgb(102, 217, 237)'
-          }}
-          outerStyle={{
-            border: '3px solid rgb(102, 217, 237)'
-          }}
-					
-				/>
-        <Wrapper>
-				  {children}
-        </Wrapper>
+        <Suspense>
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={35}
+            innerScale={1}
+            outerScale={2}
+            outerAlpha={0}
+            hasBlendMode={true}
+            innerStyle={{
+              backgroundColor: 'rgb(102, 217, 237)'
+            }}
+            outerStyle={{
+              border: '3px solid rgb(102, 217, 237)'
+            }}
+            
+          />
+          <Wrapper>
+            {children}
+          </Wrapper>
+        </Suspense>
         
 			</body>
 		</html>
